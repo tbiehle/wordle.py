c9af38858ca013cbddc6ca10da5d5a54949dbd06
Travis <travis.biehle@gmail.com>
2022-02-04 09:38:28 -0800

    restart button tweaks

--- r_button.py
+++ r_button.py
@@ -7,11 +7,11 @@ class ResetButton(Key):
         self.settings = game.settings
 
         self.text = 'RESTART'
-        self.xpos = self.settings.screen_x - 3 * (self.settings.k_width) - 8
-        self.ypos = 8
+        self.xpos = self.settings.screen_x // 2 + (3.75 * self.settings.k_width)
+        self.ypos = 13
         
         super().__init__(game, self.text, self.xpos, self.ypos)
 
         self.color = (1, 46, 26)
-
-        self.k_width = self.settings.k_width * 3
+        self.k_width = self.settings.k_width * 2.25
+        self.k_height = self.settings.k_height / 1.5
